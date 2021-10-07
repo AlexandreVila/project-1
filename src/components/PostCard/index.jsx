b054d3fc4097f40e053f9670ca37b975192b8304
Alexandre Vila <alexandrevn@alu.ufc.br>
2021-10-07 19:56:37 -0300

    Botão para paginação e organização componentes

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -1,9 +1,10 @@
-export const PostCard = ({ title, cover, body, id }) => {
+import './styles.css'
+export const PostCard = ({ title, cover, body }) => {
     return (
         <div className="post">
             <img src={cover} alt={title} />
             <div className="post-content">
-                <h1>{title} </h1>
+                <h2>{title} </h2>
                 <p>{body}</p>
             </div>
         </div>
